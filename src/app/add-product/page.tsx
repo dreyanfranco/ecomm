9e9b70ec452c64abd90a0a8b1a78134e83c7cae6
dreyanfranco <dreyanfranco@gmail.com>
2024-01-30 17:04:09 +0100

    product card and hero done

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,69 @@
+import SubmitButton from "@/components/SubmitButton";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Add Product - Ecomm",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: {
+      name,
+      description,
+      imageUrl,
+      price,
+    },
+  });
+
+  //   redirect("/");
+}
+
+export default function AddProductPage() {
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add product</h1>
+      <form action={addProduct}>
+        <input
+          name="name"
+          required
+          type="text"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          name="imageUrl"
+          required
+          type="url"
+          placeholder="Image URL"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          name="price"
+          required
+          type="number"
+          placeholder="Price"
+          className="input input-bordered mb-3 w-full"
+        />
+        <SubmitButton className="btn-block">Add Product</SubmitButton>
+      </form>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div className="text-cyan-600">Page not found</div>;
+}
