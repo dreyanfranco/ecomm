9a5d0c227df9332d9554e1e7f5e474b474a40f73
dreyanfranco <dreyanfranco@gmail.com>
2024-01-31 10:15:12 +0100

    add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function NotFound() {
-  return <div className="text-cyan-600">Page not found</div>;
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
 }
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/db/prisma";
+import { notFound } from "next/navigation";
 
 interface ProductPageProps {
   params: {
@@ -10,4 +11,5 @@ export default async function ProductPage({
   params: { id },
 }: ProductPageProps) {
   const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
 }
