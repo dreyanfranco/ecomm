fe8d0af414b1491ca00b61b5d1abe077c8796594
dreyanfranco <dreyanfranco@gmail.com>
2024-02-01 13:03:43 +0100

    handle anonymous cart items and user cart items

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -20,6 +20,7 @@
 
 // export { handler as GET, handler as POST };
 
+import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
 import { env } from "@/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -36,6 +37,17 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonymousCartIntoUserCart(user.id);
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
